e8a61a9514c2fd15ce01f70ff3394f6278e534b9
Danny Tata <dannytata@Dannys-MBP.attlocal.net>
2025-07-12 19:43:58 -0400

    Updated stg_members reprocess member tables

--- models/stg/stg_members.sql
+++ models/stg/stg_members.sql
@@ -1,8 +1,17 @@
 WITH members AS (
-    SELECT *
+    SELECT 
+    TRIM(REGEXP_SUBSTR(rawtext, '\\] (.*?):', 1, 1, 'e', 1)) AS groupmember
     FROM {{ source('whatsapp', 'rawtext')}}
+    WHERE rawtext IS NOT NULL
+),
+
+regex AS (
+    SELECT CASE
+           WHEN CHARINDEX(':', groupmember) > 0 THEN SUBSTRING(groupmember, 1, CHARINDEX(':', groupmember) - 1)
+           ELSE groupmember 
+           END AS groupmember
+from members
 )
 
-SELECT 
-TRIM(REGEXP_SUBSTR(rawtext, '\\] (.*?):', 1, 1, 'e', 1)) AS groupmember
-FROM members
+SELECT * 
+FROM regex
