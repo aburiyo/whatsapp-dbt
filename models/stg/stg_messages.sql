7e378a90deb9f4e843d676546f6e315c10467e7f
Danny Tata <dannytata@Dannys-MBP.attlocal.net>
2025-07-13 11:42:47 -0400

    Updated stg_messages to update for reprocessing

--- models/stg/stg_messages.sql
+++ models/stg/stg_messages.sql
@@ -1,11 +1,29 @@
-WITH messages AS (
+WITH MESSAGES AS (
     SELECT *
     FROM {{ source('whatsapp', 'rawtext')}}
+),
+
+REGEX AS (
+
+    SELECT 
+        TRIM(REGEXP_SUBSTR(rawtext, '\\] (.*?):', 1, 1, 'e', 1)) AS sender,
+        TRIM(REGEXP_SUBSTR(rawtext, ': (.*)', 1, 1, 'e')) AS message,
+        TRIM(REGEXP_SUBSTR(rawtext, '\\[(.*?)\\]', 1, 1, 'e')) AS datetime
+
+    FROM MESSAGES
+),
+
+PARSE AS (
+
+    SELECT CASE 
+           WHEN CHARINDEX(':', SENDER) > 0 THEN TRIM(SUBSTRING(SENDER, 1, CHARINDEX(':', SENDER) - 1))
+           ELSE SENDER 
+           END AS SENDER,
+           MESSAGE,
+           DATETIME
+    FROM REGEX
+
 )
 
-SELECT 
-TRIM(REGEXP_SUBSTR(rawtext, '\\] (.*?):', 1, 1, 'e', 1)) AS sender,
-TRIM(REGEXP_SUBSTR(rawtext, ': (.*)', 1, 1, 'e')) AS message,
-TRIM(REGEXP_SUBSTR(rawtext, '\\[(.*?)\\]', 1, 1, 'e')) AS datetime
-
-FROM messages 
+SELECT *
+FROM PARSE
