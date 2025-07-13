29254861dda5010000b4ef7e682ea5d36ed05cc2
Danny Tata <dannytata@Dannys-MBP.attlocal.net>
2025-07-12 22:00:43 -0400

    deduplicated members

--- models/stg/stg_members.sql
+++ models/stg/stg_members.sql
@@ -1,17 +1,26 @@
-WITH members AS (
+WITH MEMBERS AS (
     SELECT 
     TRIM(REGEXP_SUBSTR(rawtext, '\\] (.*?):', 1, 1, 'e', 1)) AS groupmember
     FROM {{ source('whatsapp', 'rawtext')}}
     WHERE rawtext IS NOT NULL
 ),
 
-regex AS (
-    SELECT CASE
-           WHEN CHARINDEX(':', groupmember) > 0 THEN SUBSTRING(groupmember, 1, CHARINDEX(':', groupmember) - 1)
-           ELSE groupmember 
-           END AS groupmember
-from members
+REGEX AS (
+    SELECT DISTINCT CASE
+           WHEN CHARINDEX(':', GROUPMEMBER) > 0 THEN SUBSTRING(GROUPMEMBER, 1, CHARINDEX(':', GROUPMEMBER) - 1)
+           ELSE GROUPMEMBER 
+           END AS GROUPMEMBER
+from MEMBERS
+),
+
+MEMBERORDER AS (
+    SELECT GROUPMEMBER,
+           ROW_NUMBER() OVER(ORDER BY GROUPMEMBER) AS POSITION
+    FROM REGEX
 )
 
 SELECT * 
-FROM regex
+FROM MEMBERORDER
+
+
+
