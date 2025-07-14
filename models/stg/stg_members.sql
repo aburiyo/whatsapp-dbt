818ff90606c88b64cac90eecfef54e4490fa4c0d
Danny Tata <dannytata@Dannys-MBP.attlocal.net>
2025-07-14 00:03:53 -0400

    Updated stg_members to add row number for filtering in next steps

--- models/stg/stg_members.sql
+++ models/stg/stg_members.sql
@@ -14,17 +14,7 @@ from MEMBERS
 ),
 
 MEMBERORDER AS (
-    SELECT CASE 
-           WHEN GROUPMEMBER = 'danielson' THEN 'Danny'
-           WHEN GROUPMEMBER = '+234 803 830 4509' THEN 'Ope'
-           WHEN GROUPMEMBER = 'Seyi Adu' THEN 'Seyi'
-           WHEN GROUPMEMBER = 'Azu O' THEN 'Azu'
-           WHEN GROUPMEMBER = 'Adebolu' THEN 'David'
-           WHEN GROUPMEMBER = 'BigFoot' THEN 'Victor'
-           WHEN GROUPMEMBER = 'OJ 4real' THEN 'OJ'
-           WHEN GROUPMEMBER = 'Shine William' THEN 'Shine'
-           ELSE GROUPMEMBER
-           END AS GROUPMEMBER,
+           SELECT {{ member_renaming('GROUPMEMBER') }} AS MEMBER,
            ROW_NUMBER() OVER(ORDER BY GROUPMEMBER) AS POSITION
     FROM REGEX
 )
